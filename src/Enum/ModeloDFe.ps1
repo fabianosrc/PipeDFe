bdaa8fd75b41eb103b19a1f99ac58e098f37b53c
Fabiano Silva <fabiano.msrc@outlook.com>
2026-08-02 22:32:46 -0300

    feat(enum): define supported DFe models

---
+++ src/Enum/ModeloDFe.ps1
@@ -0,0 +1,9 @@
+﻿enum ModeloDFe
+{
+    NFe   = 55 # Nota Fiscal Eletrônica
+    CTe   = 57 # Conhecimento de Transporte Eletrônico
+    MDFe  = 58 # Manifesto Eletrônico de Documentos Fiscais
+    NFCom = 62 # Nota Fiscal Fatura de Serviços de Comunicação Eletrônica
+    NFCe  = 65 # Nota Fiscal de Consumidor Eletrônica
+    CTeOS = 67 # Conhecimento de Transporte Outros Serviços
+}
