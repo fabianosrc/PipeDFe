973c594bfb717caaa063e6da3b5a10e0ade17a98
Fabiano Silva <fabiano.msrc@outlook.com>
2026-08-08 14:31:55 -0300

    feat(domain): add DFeEvento enum

---
+++ src/Enum/DFeEvento.ps1
@@ -0,0 +1,23 @@
+﻿enum DFeEvento
+{
+    CartaCorrecao
+    Cancelamento
+    CancelamentoSubstituicao
+    AtorInteressadoTransportador
+
+    PedidoProrrogacao1Prazo
+    PedidoProrrogacao2Prazo
+    CancelamentoProrrogacao1Prazo
+    CancelamentoProrrogacao2Prazo
+
+    RegistroMultimodal
+    InformacoesGTV
+    ComprovanteEntrega
+    CancelamentoComprovanteEntrega
+
+    Encerramento
+    InclusaoCondutor
+    InclusaoDFe
+    PagamentoOperacaoTransporte
+    AlteracaoPagamentoTransporte
+}
